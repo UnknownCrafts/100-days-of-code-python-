658659e49940169a89623d2700d9a8d45f0142a2
Surya Vasudev <suryavasudev05@gmail.com>
2024-06-13 12:05:09 -0400

    first commit?

---
+++ Day1.py
@@ -0,0 +1,10 @@
+# Band name generator
+
+print("Welcome to the Band Name Generator\n")
+
+city_name = input("What's the name of the city you grew up in?\n")
+print("")
+pet_name = input("What's your pet's name?\n")
+print("")
+print("Your band name could be " + city_name + " " + pet_name)
+
