d2a608fb6315daced307bf3e77fe61a12c747369
Surya Vasudev <suryavasudev05@gmail.com>
2024-06-17 20:28:17 -0400

    small refactor to simplify the code

--- Beginner projects/Day 014 code/Day14.py
+++ Beginner projects/Day 014 code/Day14.py
@@ -12,6 +12,29 @@ choice_A = random.choice(gamedata.data)
 choice_B = random.choice(gamedata.data)
 score = 0
 
+def choice_comparision():
+    global repeat_flag, choice_A, choice_B, score
+    
+    if user_choice == "A":
+        if choice_A["follower_count"] > choice_B["follower_count"]:
+                score += 1
+                repeat_flag = True
+                choice_B = random.choice(gamedata.data)
+                while choice_A == choice_B:
+                    choice_B = random.choice(gamedata.data)
+        else:
+            repeat_flag = False
+    else:
+        if choice_B["follower_count"] > choice_A["follower_count"]:
+                score += 1
+                repeat_flag = True
+                choice_A = choice_B
+                choice_B = random.choice(gamedata.data)
+                while choice_A == choice_B:
+                    choice_B = random.choice(gamedata.data)
+        else:
+            repeat_flag = False
+
 print(f"Compare A: {choice_A.get('name')}, a {choice_A.get('description')}, from {choice_A.get('country')}.")
 print("")
 print("")
@@ -19,25 +42,7 @@ print(art.vs)
 print(f"Against B: {choice_B.get('name')}, a {choice_B.get('description')}, from {choice_B.get('country')}.")
 user_choice = input("Who has more followers? Type 'A' or 'B': ").capitalize().strip()
 
-if user_choice == "A":
-    if choice_A["follower_count"] > choice_B["follower_count"]:
-        score += 1
-        repeat_flag = True
-        choice_B = random.choice(gamedata.data)
-        while choice_A == choice_B:
-            choice_B = random.choice(gamedata.data)
-    else:
-        repeat_flag = False
-else:
-    if choice_B["follower_count"] > choice_A["follower_count"]:
-        score += 1
-        repeat_flag = True
-        choice_A = choice_B
-        choice_B = random.choice(gamedata.data)
-        while choice_A == choice_B:
-            choice_B = random.choice(gamedata.data)
-    else:
-        repeat_flag = False
+choice_comparision()
 
 while repeat_flag:
     
@@ -51,25 +56,7 @@ while repeat_flag:
     print(f"Against B: {choice_B.get('name')}, a {choice_B.get('description')}, from {choice_B.get('country')}.")
     user_choice = input("Who has more followers? Type 'A' or 'B': ").capitalize().strip()
 
-    if user_choice == "A":
-        if choice_A["follower_count"] > choice_B["follower_count"]:
-            score += 1
-            repeat_flag = True
-            choice_B = random.choice(gamedata.data)
-            while choice_A == choice_B:
-                choice_B = random.choice(gamedata.data)
-        else:
-            repeat_flag = False
-    else:
-        if choice_B["follower_count"] > choice_A["follower_count"]:
-            score += 1
-            repeat_flag = True
-            choice_A = choice_B
-            choice_B = random.choice(gamedata.data)
-            while choice_A == choice_B:
-                choice_B = random.choice(gamedata.data)
-        else:
-            repeat_flag = False
+    choice_comparision()
 
 os.system("cls||clear")
 print(art.logo)
