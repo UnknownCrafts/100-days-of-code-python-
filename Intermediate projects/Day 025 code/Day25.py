1a32850c545a7d2b86303961f95997bd1a857892
Surya Vasudev <suryavasudev05@gmail.com>
2024-06-21 23:08:20 -0400

    Day 25 placeholder code, plus reset highscore.txt

---
+++ Intermediate projects/Day 025 code/Day25.py
@@ -0,0 +1,13 @@
+# U.S States Naming Game using turtle
+
+import turtle
+
+
+screen = turtle.Screen()
+
+screen.title("U.S States Naming Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+screen.exitonclick()
