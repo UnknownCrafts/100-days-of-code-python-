1e6f365dd10c6a1d815f5073f2cf183cf8e36645
Surya Vasudev <suryavasudev05@gmail.com>
2024-06-23 21:54:54 -0400

    Day25 code

--- Intermediate projects/Day 025 code/Day25.py
+++ Intermediate projects/Day 025 code/Day25.py
@@ -1,6 +1,7 @@
 # U.S States Naming Game using turtle
 
 import turtle
+import pandas
 
 
 screen = turtle.Screen()
@@ -10,4 +11,37 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 
 turtle.shape(image)
-screen.exitonclick()
+
+writer_turtle = turtle.Turtle()
+writer_turtle.hideturtle()
+writer_turtle.penup()
+
+correct_guesses = []
+csv = pandas.read_csv("50_states.csv")
+all_states = csv["state"].to_list()
+count = 0
+
+while len(correct_guesses) < 50:
+
+    answer = screen.textinput(title=f"{count}/50 states correct", prompt="What's another state's name? ").title().strip()
+    
+    if answer == "Exit":
+        break
+    
+    if answer in all_states and answer not in correct_guesses:
+        correct_guesses.append(answer)
+        mask = csv['state'].values == answer
+        x_value = csv[mask]["x"].iloc[0]
+        y_value = csv[mask]["y"].iloc[0]
+        writer_turtle.goto((x_value, y_value))
+        writer_turtle.write(answer)
+        count += 1
+
+missed_states = {'states': []}
+for state in all_states:
+    
+    if state not in correct_guesses:
+        missed_states['states'].append(state)
+
+df = pandas.DataFrame(missed_states)
+df.to_csv('states_to_learn.csv')
