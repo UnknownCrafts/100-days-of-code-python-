f12813a67bbc123b09791062e7375f3fb0476e43
Surya Vasudev <suryavasudev05@gmail.com>
2024-06-25 13:57:07 -0400

    Added GUI and saving password logic plus gitignore changes

---
+++ Intermediate projects/Day 029 code/Day29.py
@@ -0,0 +1,63 @@
+# Python password manager using tkinter
+
+from tkinter import *
+from tkinter import messagebox
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_input.get()
+    email = email_input.get()
+    password = password_input.get()
+    messagebox_message = "These are the details that are entered: \nEmail: " + email + "\nPassword: " + password + "\nIs it ok to save?"
+    
+    if len(website.strip()) == 0 or len(password.strip() == 0 or len(email.strip()) == 0):
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+    else:
+        answer = messagebox.askokcancel(title=website, message=messagebox_message)
+        if answer:
+            with open("data.txt", mode="a") as file:
+                file.write(website + " | " + email + " | " + password + "\n")
+            website_input.delete(0, END)
+            password_input.delete(0, END)
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+window.eval('tk::PlaceWindow . center')
+
+logo = PhotoImage(file="logo.png")
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+canvas.create_image(100, 100, image=logo)
+canvas.grid(column=1, row=0)
+
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+website_input = Entry(width=35, justify="left")
+website_input.grid(column=1, row=1, columnspan=2)
+website_input.focus()
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+email_input = Entry(width=35, justify="left")
+email_input.grid(column=1, row=2, columnspan=2)
+email_input.insert(0, "dummy@email.com")
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+password_input = Entry(width=21, justify="left")
+password_input.grid(column=1, row=3, padx=12)
+
+generate_pass_button = Button(text="Generate Password", width=11)
+generate_pass_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(column=1, row=4, columnspan=2)
+
+window.mainloop()
