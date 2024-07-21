ae9e23854c73333b4c75199e98887c011c1aa0da
Surya Vasudev <suryavasudev05@gmail.com>
2024-07-21 15:37:18 -0400

    Day 45 code finished

---
+++ Intermediate+ projects/Day 045 code/Top 100 movies (web scrap)/main.py
@@ -0,0 +1,19 @@
+# More webscraping practice
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+# Write your code below this line 👇
+
+response = requests.get(URL)
+
+webpage = response.text
+
+soup = BeautifulSoup(webpage, "html.parser")
+
+movie_titles = [f"{x.getText()}\n" for x in soup.find_all(name="h3", class_="title")][::-1]
+
+with open("movies.txt", "w") as file:
+    file.writelines(movie_titles)
