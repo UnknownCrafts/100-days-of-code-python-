c88b7c53f37d689b0e31f1c3c1e67ce48c7ba81b
Surya Vasudev <suryavasudev05@gmail.com>
2024-08-09 17:51:25 -0400

    partial day 68 code

---
+++ 04 Advanced projects/Day 068 code/flask-auth/main.py
@@ -0,0 +1,102 @@
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# CREATE DATABASE
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+# CREATE TABLE IN DB
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(1000))
+    is_authenticated = False
+    is_active = False
+    is_anonymous = False
+    
+    def get_id():
+        return id
+
+
+with app.app_context():
+    db.create_all()
+    
+@login_manager.user_loader
+def load_user(id):
+    return User.get(id)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        new_user = User(
+            email = request.form.get("email"),
+            name = request.form.get("name"),
+            password = generate_password_hash(request.form.get("password"), method="pbkdf2:sha256", salt_length=8)
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return render_template("secrets.html", name=new_user.name)
+    
+    return render_template("register.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        user = User(
+            email = request.form.get("email"),
+            password = request.form.get("password"),
+            
+        )
+        login_user()
+    return render_template("login.html")
+
+
+@app.route('/secrets')
+@login_required
+def secrets():
+    return render_template("secrets.html")
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    pass
+
+
+@app.route('/download')
+@login_required
+def download():
+    return send_from_directory(
+        "static",
+        path="files/cheat_sheet.pdf"
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
